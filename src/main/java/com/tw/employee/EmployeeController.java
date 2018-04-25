dc2377f01b51da7bf48ac830fd2872ae345d2139
zzp <2269225970@qq.com>
2018-04-25 13:08:25 +0800

    add Employee and EmployeeController

--- src/main/java/com/tw/employee/EmployeeController.java
+++ src/main/java/com/tw/employee/EmployeeController.java
@@ -3,15 +3,32 @@ package com.tw.employee;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
 public class EmployeeController {
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    private List<Employee> employees;
+
+    public static final List<Employee> employeeList =  Arrays.asList(
+            new Employee(1, "小明", 20, "男"),
+            new Employee(2, "小红", 19, "女"),
+            new Employee(3, "小智", 15, "男"),
+            new Employee(4, "小刚", 16, "男"),
+            new Employee(5, "小霞", 15, "女")
+    );
+
+    public EmployeeController() {
+        this.employees = employeeList;
     }
 
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Employee> getEmployees() {
+        return employees;
+    }
 }
