c35d4cee3a3cdc33132153caaa3108df086233f3
zzp <2269225970@qq.com>
2018-04-25 10:13:09 +0800

    first commit

---
+++ src/main/java/com/tw/employee/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.tw.employee;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class EmployeeController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
